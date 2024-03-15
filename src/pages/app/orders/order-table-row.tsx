dedde49861637638b27f948b9b7f739828aeff3e
tiala.rocha <tiala.rocha@compasso.com.br>
2024-03-15 09:19:18 -0300

    feat: Component: Order Details

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,7 +1,10 @@
 import { ArrowRight, Search, X } from 'lucide-react'
 
 import { Button } from '@/componets/ui/button'
+import { Dialog, DialogTrigger } from '@/componets/ui/dialog'
 import { TableCell, TableRow } from '@/componets/ui/table'
+
+import { OrderDetails } from './order-details'
 
 // interface OrderTableRowProps {}
 
@@ -9,29 +12,47 @@ export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              {/* className="sr-only" -> Acessibilidade -> serve p leitores de tela */}
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          {/* OrderDetails -> componente que contem todas as informacoes do 
+          pedido (DialogContent) 
+          Aqui o componente DialogContent foi abstraido desse arquivo para 
+          que o mesmo nao ficasse enorme ocupando muito espaco nesse arquivo */}
+          <OrderDetails />
+        </Dialog>
       </TableCell>
+
       <TableCell className="font-mono text-xs font-medium">
         821e78f7asdhdf128h
       </TableCell>
+
       <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+
       <TableCell>
         <div className="flex items-center gap-2">
           <span className="h-2 w-2 rounded-full bg-slate-400" />
           <span className="font-medium text-muted-foreground">Pendente</span>
         </div>
       </TableCell>
+
       <TableCell className="font-medium">Diego Schell Fernandes</TableCell>
+
       <TableCell className="font-medium">R$ 149,90</TableCell>
+
       <TableCell>
         <Button variant="outline" size="xs">
           <ArrowRight className="mr-2 h-3 w-3" />
           Aprovar
         </Button>
       </TableCell>
+
       <TableCell>
         <Button variant="ghost" size="xs">
           <X className="mr-2 h-3 w-3" />
